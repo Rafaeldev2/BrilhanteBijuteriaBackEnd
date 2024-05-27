d44406615b1177fa10d08aa7f360abcb95843065
Rafaeldev2 <116116255+Rafaeldev2@users.noreply.github.com>
2024-05-27 17:48:33 -0300

    Começo do BackEnd do Projeto Brilhante Bijuteria

---
+++ ProjetoBijuteria/src/test/java/com/brilhante/bijuteria/BijuteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brilhante.bijuteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BijuteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
